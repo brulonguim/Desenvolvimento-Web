1bb766c5847f271413f04008602d1b1206206ce0
brulonguim <bruninha.longuim@gmail.com>
2024-09-20 10:03:20 -0300

    Atualização do js

--- lab32/script.js
+++ lab32/script.js
@@ -4,11 +4,15 @@ function calcularPontuacao() {
     const q1 = document.querySelector('input[name="q1"]:checked');
     const q2 = document.querySelector('input[name="q2"]:checked');
     const q3 = document.querySelector('input[name="q3"]:checked');
+    const q4 = document.querySelector('input[name="q4"]:checked');
+    const q5 = document.querySelector('input[name="q5"]:checked');
     
   
     if (q1) pontuacao += parseInt(q1.value);
     if (q2) pontuacao += parseInt(q2.value);
     if (q3) pontuacao += parseInt(q3.value);
+    if (q4) pontuacao += parseInt(q4.value);
+    if (q5) pontuacao += parseInt(q5.value);
     
   
     document.getElementById("resultado").innerHTML = "Você acertou " + pontuacao + " de 4 perguntas.";
